0809fa38ce016315a89ece27c6597477f958fc08
joelcoxokc <joel.cox.dev@gmail.com>
2015-12-08 22:52:22 -0800

    initialcommit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,37 @@
-export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
-    ]);
+import 'bootstrap/js/bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+import appTemplate from 'app.html!text';
 
-    this.router = router;
+function templateContent(str) {
+  let fragment;
+  let template;
+  let content;
+
+  fragment = document.createElement('div');
+  fragment.innerHTML = str;
+  template = fragment.firstChild;
+  fragment.remove();
+  content  = document.importNode(template.content, true);
+  return content;
+}
+
+export class App {
+
+  title = 'ES6 Core';
+  constructor(rootElement) {
+    this.element = rootElement;
+    this.setTitle();
+  }
+
+  setTitle() {
+    let content = templateContent(appTemplate);
+    let H1      = content.querySelector('#title');
+
+    H1.innerHTML = this.title;
+    this.insertNode(content);
+  }
+
+  insertNode(node) {
+    this.element.insertBefore(node, this.element.firstChild);
   }
 }
